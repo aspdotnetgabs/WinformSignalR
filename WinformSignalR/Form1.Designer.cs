0fafa1980356646f7ef80e32418c6835fe41f267
Bernard Gabon <hewbertgabon@gmail.com>
2020-03-24 18:56:27 +0800

    init

---
+++ WinformSignalR/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WinformSignalR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(34, 71);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(469, 340);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(142, 28);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(219, 28);
+            this.txtUsername.TabIndex = 2;
+            this.txtUsername.Text = "johnnysins";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(383, 24);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(120, 38);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(412, 423);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(91, 38);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(34, 428);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(362, 28);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 482);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Chat [SignalR]";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ WinformSignalR/Form1.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformSignalR
+{
+    public partial class Form1 : Form
+    {
+        //Connection to a SignalR server
+        HubConnection _signalRConnection;
+
+        //Proxy object for a hub hosted on the SignalR server
+        IHubProxy _hubProxy;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task ConnectAsync()
+        {
+            // https://stackoverflow.com/questions/15516014/is-it-good-idea-to-use-control-checkforillegalcrossthreadcalls-false
+            ListBox.CheckForIllegalCrossThreadCalls = false;
+
+            // SignalR server url and hub name. Alt url https://signalrgabs.azurewebsites.net
+            string host = "http://signalrfree.bernardgabon.com"; 
+            string hubName = "MessageHub";
+
+            _signalRConnection = new HubConnection(host);
+            _signalRConnection.StateChanged += HubConnection_StateChanged;
+
+            _hubProxy = _signalRConnection.CreateHubProxy(hubName);
+
+            // Register to the "broadcastMessage" callback method of the hub
+            // This method is invoked by the hub
+            _hubProxy.On<string, string>("broadcastMessage", (name, message) => ReceiveMessage(name, message) );
+
+            btnConnect.Enabled = false;
+
+            try
+            {
+                // Connect to the server
+                await _signalRConnection.Start();
+
+                // Send user name for this client, so we won't need to send it with every message
+                await _hubProxy.Invoke("Send", txtUsername.Text, " Joining the chat... ");
+            }
+            catch
+            {
+                btnConnect.Enabled = true;
+            }
+        }
+
+        private void HubConnection_StateChanged(StateChange scObj)
+        {
+            if (scObj.NewState == Microsoft.AspNet.SignalR.Client.ConnectionState.Connected)
+            {
+                MessageBox.Show("Connected.");
+                txtUsername.Enabled = false;
+                this.AcceptButton = btnSend;
+                btnConnect.Enabled = false;
+                btnSend.Enabled = true;
+                txtMessage.Focus();          
+            }
+            else if (scObj.NewState == Microsoft.AspNet.SignalR.Client.ConnectionState.Disconnected)
+            {
+                this.Text += " [Disconnected]";
+                btnSend.Enabled = false;
+                btnConnect.Enabled = true;
+            }
+        }
+
+        private void ReceiveMessage(string from, string msg)
+        {
+            listBox1.Items.Add($"{from}: {msg}");
+        }
+
+        // Make this button event "async"
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            await ConnectAsync();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // Call the "Send" method on the hub (on the server) with the given parameters
+            _hubProxy.Invoke("Send", txtUsername.Text, txtMessage.Text).Wait();
+            txtMessage.Text = string.Empty;
+        }
+    }
+}
